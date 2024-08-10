86e66eb59e3d22fc0cbf13f086af64c5bc46855c
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-10 09:35:59 +0600

    create object and implement method and some buildtin method and key and value

---
+++ obj.js
@@ -0,0 +1,28 @@
+const obj ={
+  firstName:'monir',
+  age:12,
+  getName:function() {
+    this.firstName+this.lastName
+    
+  },
+  "address:'rohimpur'
+}
+
+obj.lastName="Hossain";
+console.log(obj["address"])
+obj.getName();
+delete obj["address"]
+obj.keys()
+obj.values()
+
+
+
+// access o(1)
+// delete o(1)
+// insert o(1)
+
+
+//search o(n)
+// object.keys() o(n)
+// object.values() o(n)
+// object.entries() o(n)
