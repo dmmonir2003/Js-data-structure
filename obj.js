1b47ef4e30d1964f5ef6b4b128a0786b975ce7a1
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-10 14:59:08 +0600

    update obj.js some change

--- obj.js
+++ obj.js
@@ -14,15 +14,15 @@ obj.getName();
 delete obj["address"];
 obj.keys();
 obj.values();
-
+// 
 
 
 // access o(1)
 // delete o(1)
 // insert o(1)
 
-
 //search o(n)
 // object.keys() o(n)
 // object.values() o(n)
 // object.entries() o(n)
+//
