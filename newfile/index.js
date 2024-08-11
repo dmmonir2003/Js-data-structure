4f60558aa476c9be0d0546c7be0030d133511031
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-11 06:30:23 +0600

    update index.js implement set building method

--- newfile/index.js
+++ newfile/index.js
@@ -1,3 +1,19 @@
 // set implement 
 
 const set=new Set([1,2,3]);
+
+
+// implement set building method
+
+set.add(4);
+set.has(4);
+console.log(set.keys(),'key');
+set.delete(1);
+
+
+
+for (let i of set) {
+  console.log(i);
+};
+
+set.clear()
