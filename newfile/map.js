8eb7efd7493f5cf37a712eb30cf9932514ae550f
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-12 06:27:29 +0600

    implement map common builtin  method

--- newfile/map.js
+++ newfile/map.js
@@ -1,7 +1,16 @@
 const map = new Map([['a',1],['b',2]]);
 
+
+//buildin method
+map.set('c',20);
+map.delete('b');
+console.log(map.has('b'));
+console.log(map.size);
+
+
 for (let [key,value] of map) {
   // Tab to edit
   
   console.log(`${key}: ${value}`);
 }
+
