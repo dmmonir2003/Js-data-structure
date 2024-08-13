69115d7a0de8da32a523c90532355a9c71ecb056
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-13 06:23:58 +0600

    update stack.js check all method and implement clear and print method for testing

--- newfile/stack.js
+++ newfile/stack.js
@@ -27,6 +27,33 @@ class Stack {
   }
   
   isEmpty(){
-    return this.getSize()===0;
+    return this.getSize() === 0;
+  }
+  
+  clear(){
+    return this.items=[];
+  }
+  
+  print(){
+    for (let item of this.items) {
+      // Tab to edit
+      console.log(item)
+    }
   }
 }
+
+
+const stack = new Stack();
+
+stack.push('rohim');
+stack.push('korim');
+stack.push('Abdullah');
+stack.push('monir');
+stack.print()
+console.log('after pop');
+stack.pop();
+console.log(stack.getSize(),'size')
+console.log('isEmpty',stack.isEmpty());
+
+stack.clear();
+stack.print()
