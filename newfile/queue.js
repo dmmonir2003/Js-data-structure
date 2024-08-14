45dbb46b970a72fc1f2af8026d64be93a56b4f66
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-14 06:10:13 +0600

    create queue dsa

--- newfile/queue.js
+++ newfile/queue.js
@@ -1,1 +1,31 @@
-console.log('queue');
+class Queue{
+  constructor(){
+    this.items=[];
+  }
+  enqueue(item){
+    this.items.push(item)
+  }
+  
+  dequeue(){
+    return this.items.shift()
+  }
+  
+  isEmpty(){
+    return this.items.length===0;
+  }
+  peek(){
+    if (!this.isEmpty()) {
+      return this.items[0];
+   }
+   
+   return null;
+   
+  }
+  
+  size(){
+    return this.items.length()
+  }
+  
+  
+  
+}
