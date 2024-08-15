5fa20d814236336c7701ccd4d5dc0168994599b4
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-15 06:05:56 +0600

    implement enqueue dequeue peek size isEmpty and print method

--- newfile/optimaizeQueue.js
+++ newfile/optimaizeQueue.js
@@ -5,5 +5,35 @@ class Queue{
     this.rear=0;
   }
   
+  emqueue(item){
+    this.items[this.rear]=item;
+    this.rear++
+  }
+  
+  dequeue(){
+    const item=this.items[this.front]
+    delete this.items[this.front]
+    this.front++;
+    return item
+  }
+
+  isEmpty(){
+    return this.rear-this.front===0
+  }
+  
+  peek(){
+    
+    return this.items[this.front]
+    
+  }
+
+
+  size(){
+    return this.rear-this.front 
+  }
+  
+  print(){
+    console.log(this.items)
+  }
 
 }
