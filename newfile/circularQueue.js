d593b61d08c8c784a4b96db250f9d2d9581554bd
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-17 07:00:21 +0600

    update circularQueue.js create enqueue dequeue peek and print method

--- newfile/circularQueue.js
+++ newfile/circularQueue.js
@@ -7,5 +7,61 @@ class CircularQueue{
     this.front= -1;
   }
   
+  isFull(){
+    return this.currentLeangth===this.capacity;
+  }
   
+  isEmpty(){
+    return currentLeangth===0;
+  }
+  
+  enqueue(element){
+    if (!this.isFull()) {
+      this.rear=(this.rear+1)%this.capacity;
+      this.items[this.rear]=element;
+      this.currentLeangth+=1;
+      if (this.front===-1) {
+        this.front =this.rear
+      }
+    }
+  }
+  
+  dequeue(){
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item=this.items[this.front];
+    this.items[this.front]=null;
+    this.front=(this.rear+1)%this.capacity;
+    this.currentLeangth-=1;
+    if (this.isEmpty()) {
+      this.front= -1;
+      this.rear-=1;
+    }
+    return item;
+  }
+  
+  peek(){
+    if (!this.isEmpty()) {
+      return this.items[this.front]
+    }
+    return null
+  }
+  
+  print(){
+    if (this.isEmpty()) {
+      return console.log('queue is empty')
+    } else {
+    let i;
+    let str='';
+     
+     for(i=this.front;i!==this.rear;i=(i+1)%this.capacity){
+       
+       str+=this.items[i]+ ' ';
+     }
+     
+     str+=this.items[i];
+     console.log(str)
+    }
+  }
 }
