6d2966da248f859628a289d48bb50b3337ae2c0e
dmmonir2003 <dmmonir2003@gmail.com>
2024-08-17 07:21:25 +0600

    update circularQueue.js test all method

--- newfile/circularQueue.js
+++ newfile/circularQueue.js
@@ -1,67 +1,82 @@
-class CircularQueue{
-  constructor(capacity){
-    this.items=new Array(capacity);
-    this.capacity=capacity;
-    this.currentLeangth=0;
-    this.rear= -1;
-    this.front= -1;
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLeangth = 0;
+    this.rear = -1;
+    this.front = -1;
   }
-  
-  isFull(){
-    return this.currentLeangth===this.capacity;
+
+  isFull() {
+    return this.currentLeangth === this.capacity;
   }
-  
-  isEmpty(){
-    return currentLeangth===0;
+
+  isEmpty() {
+    return this.currentLeangth === 0;
   }
-  
-  enqueue(element){
+
+  enqueue(element) {
     if (!this.isFull()) {
-      this.rear=(this.rear+1)%this.capacity;
-      this.items[this.rear]=element;
-      this.currentLeangth+=1;
-      if (this.front===-1) {
-        this.front =this.rear
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = element;
+      this.currentLeangth += 1;
+      if (this.front === -1) {
+        this.front = this.rear
       }
     }
   }
-  
-  dequeue(){
+
+  dequeue() {
     if (this.isEmpty()) {
       return null;
     }
-    const item=this.items[this.front];
-    this.items[this.front]=null;
-    this.front=(this.rear+1)%this.capacity;
-    this.currentLeangth-=1;
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front+ 1) % this.capacity;
+    this.currentLeangth -= 1;
     if (this.isEmpty()) {
-      this.front= -1;
-      this.rear-=1;
+      this.front = -1;
+      this.rear = -1;
     }
     return item;
   }
-  
-  peek(){
+
+  peek() {
     if (!this.isEmpty()) {
       return this.items[this.front]
     }
     return null
   }
-  
-  print(){
+
+  print() {
     if (this.isEmpty()) {
       return console.log('queue is empty')
     } else {
-    let i;
-    let str='';
-     
-     for(i=this.front;i!==this.rear;i=(i+1)%this.capacity){
-       
-       str+=this.items[i]+ ' ';
-     }
-     
-     str+=this.items[i];
-     console.log(str)
+      let i;
+      let str = '';
+
+      for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+
+        str += this.items[i] + ' ';
+      }
+
+      str += this.items[i];
+      console.log(str)
     }
   }
 }
+
+
+const queue = new CircularQueue(5);
+console.log(queue.isEmpty())
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+queue.print()
+
+console.log(queue.dequeue())
+console.log(queue.peek())
+queue.enqueue(60)
+queue.print()
