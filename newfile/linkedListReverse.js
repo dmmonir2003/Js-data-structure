f331baf7e11fabe5b1c4daae89f949ee0c57abd3
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-10 05:56:38 +0600

    update linkedListReverse.js input then create  new nodes

--- newfile/linkedListReverse.js
+++ newfile/linkedListReverse.js
@@ -17,3 +17,9 @@ function reverseLinkedList(head) {
     }
     return prev;
 }
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
