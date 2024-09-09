9503dc399da5a14eb04d509b71f90963277ef383
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-10 05:58:03 +0600

    update linkedListReverse.js call reverse function

--- newfile/linkedListReverse.js
+++ newfile/linkedListReverse.js
@@ -23,3 +23,7 @@ head.next = new ListNode(2);
 head.next.next = new ListNode(3);
 head.next.next.next = new ListNode(4);
 head.next.next.next.next = new ListNode(5);
+
+let reversedHead = reverseLinkedList(head);
+
+
