a2359f8f4a5eb48bd6568ac0b5e2d119eec3ba58
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-12 06:38:39 +0600

    update linkedListedStack.js implement linkedList Stack

---
+++ newfile/linkedListedStack.js
@@ -0,0 +1,50 @@
+// Node class to create new nodes
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// Stack class using a linked list
+class Stack {
+  constructor() {
+    this.top = null; // The top of the stack
+    this.size = 0; // Size of the stack
+  }
+
+  // Push a value onto the stack
+  push(value) {
+    const newNode = new Node(value);
+    if (this.top) {
+      newNode.next = this.top;
+    }
+    this.top = newNode;
+    this.size++;
+  }
+
+  // Pop a value from the stack
+  pop() {
+    if (!this.top) return null; // Stack is empty
+    const valueToPop = this.top.value;
+    this.top = this.top.next;
+    this.size--;
+    return valueToPop;
+  }
+
+  // Peek at the top value of the stack
+  peek() {
+    if (!this.top) return null; // Stack is empty
+    return this.top.value;
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // Get the size of the stack
+  getSize() {
+    return this.size;
+  }
+}
