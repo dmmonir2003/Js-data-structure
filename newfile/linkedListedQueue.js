68abe3dd07be136fe29cd824ec7a24d871145017
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-13 09:18:01 +0600

    implement linkdList queue code in js file

---
+++ newfile/linkedListedQueue.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null; // Points to the front of the queue
+    this.rear = null;  // Points to the rear of the queue
+    this.size = 0;     // Keeps track of the size of the queue
+  }
+
+  // Enqueue: Add an element to the end of the queue
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.rear) {
+      this.front = this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+    this.size++;
+  }
+
+  // Dequeue: Remove an element from the front of the queue
+  dequeue() {
+    if (!this.front) {
+      return null; // Queue is empty
+    }
+    const dequeuedNode = this.front;
+    this.front = this.front.next;
+    if (!this.front) {
+      this.rear = null; // If the queue is now empty
+    }
+    this.size--;
+    return dequeuedNode.value;
+  }
+
+  // Peek: View the front element of the queue
+  peek() {
+    if (!this.front) {
+      return null;
+    }
+    return this.front.value;
+  }
+
+  // Check if the queue is empty
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // Get the size of the queue
+  getSize() {
+    return this.size;
+  }
+}
