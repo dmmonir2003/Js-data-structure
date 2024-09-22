aaad5631f72d9be38095598c85de7c3012acffeb
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-22 06:03:29 +0600

    implement binary tree search example code use javascript

---
+++ newfile/binaryTreeSearch.js
@@ -0,0 +1,64 @@
+// Define the Node class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Define the BinarySearchTree class
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Insert a new node into the tree
+  insert(data) {
+    const newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Search for a value in the tree
+  search(node, data) {
+    if (node === null) {
+      return null;
+    }
+    if (data < node.data) {
+      return this.search(node.left, data);
+    } else if (data > node.data) {
+      return this.search(node.right, data);
+    } else {
+      return node;
+    }
+  }
+
+  // In-order traversal (left, root, right)
+  inorder(node) {
+    if (node !== null) {
+      this.inorder(node.left);
+      console.log(node.data);
+      this.inorder(node.right);
+    }
+  }
+}
