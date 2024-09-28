612bfde5db70356cc6399b393411dc801c4c46a1
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-28 07:39:25 +0600

    implement binarySearchTree max and min value code

---
+++ newfile/binarySearchTreeMaxMin.js
@@ -0,0 +1,53 @@
+// Node class for the tree
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// Binary Search Tree class
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Insert a value into the tree
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    // Find the maximum value in the tree
+    findMax() {
+        if (this.root === null) {
+            return null;
+        }
+        let current = this.root;
+        while (current.right !== null) {
+            current = current.right;
+        }
+        return current.value;
+    }
+}
