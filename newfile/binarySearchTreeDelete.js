b8cf7cbc9b3cd0b74b7c1de58b7f5883f3f1da0a
dmmonir2003 <dmmonir2003@gmail.com>
2024-09-30 20:45:30 +0600

    update binarySearchTreeDelete.js insert data in bst

--- newfile/binarySearchTreeDelete.js
+++ newfile/binarySearchTreeDelete.js
@@ -99,3 +99,15 @@ class BinarySearchTree {
     return this.root;
   }
 }
+
+
+// Example Usage:
+const bst = new BinarySearchTree();
+
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
