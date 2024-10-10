ac5bad769817bd8037ad7853562d3851cc85d1d0
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-10 17:01:30 +0600

    implement  graphDisplayAndHasedge code

---
+++ newfile/graphDisplayAndHasedge.js
@@ -0,0 +1,33 @@
+// Graph represented as an adjacency list
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  // Add a vertex (node)
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  // Add an edge between two vertices
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1); // For undirected graph
+    }
+  }
+
+  // Check if an edge exists between two vertices
+  hasEdge(vertex1, vertex2) {
+    return this.adjacencyList[vertex1].includes(vertex2);
+  }
+
+  // Display the graph
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(`${vertex} -> ${this.adjacencyList[vertex].join(", ")}`);
+    }
+  }
+}
