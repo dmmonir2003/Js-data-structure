48e3226e5631af0aeef79597c5f158defd133827
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-10 17:02:31 +0600

    insert data in graph

--- newfile/graphDisplayAndHasedge.js
+++ newfile/graphDisplayAndHasedge.js
@@ -31,3 +31,13 @@ class Graph {
     }
   }
 }
+
+
+// Example usage:
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
