87ad41f6c548c47019d12b9abdf1c0042796698b
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-20 23:42:21 +0600

    update toBeOrnotToBe.js code implement

---
+++ newfile/problem-solving /toBeOrnotToBe.js
@@ -0,0 +1,17 @@
+function expect(val) {
+    return {
+        toBe: function(otherVal) {
+            if (val === otherVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(otherVal) {
+            if (val !== otherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
