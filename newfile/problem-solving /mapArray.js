c726c7fba5e36457048cea48f35267d608b73274
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-23 15:56:56 +0600

    update mapArray.js test the code

--- newfile/problem-solving /mapArray.js
+++ newfile/problem-solving /mapArray.js
@@ -8,3 +8,10 @@ function mapArray(arr, fn) {
   return result;
 }
 
+// Example usage:
+const arr = [1, 2, 3, 4];
+const fn = (element, index) => element * 2 + index;
+
+const transformedArray = mapArray(arr, fn);
+console.log(transformedArray); // Output: [2, 5, 8, 11]
+
