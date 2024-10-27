538e91986d3738637516fc7006eeb79833a6c782
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-28 00:27:38 +0600

    update argumentsLength.js test the code

--- newfile/problem-solving /argumentsLength.js
+++ newfile/problem-solving /argumentsLength.js
@@ -1,3 +1,7 @@
 var argumentsLength = function(...args) {
     return args.length;
 };
+
+console.log(argumentsLength(1, 2, 3)); // Output: 3
+console.log(argumentsLength("a", "b", "c", "d")); // Output: 4
+console.log(argumentsLength()); // Output: 0
