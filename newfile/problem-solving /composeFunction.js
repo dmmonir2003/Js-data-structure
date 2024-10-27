ab2157a3b2e855b9b6654873362facf42eeed1c3
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-27 17:03:37 +0600

    update composeFunction.js test this code

--- newfile/problem-solving /composeFunction.js
+++ newfile/problem-solving /composeFunction.js
@@ -14,3 +14,12 @@ var compose = function(functions) {
         return functions.reduceRight((acc, fn) => fn(acc), x);
     };
 };
+
+const fn = compose([x => x + 1, x => x * 2, x => x - 3]);
+console.log(fn(4));  // Output: 5
+
+// Explanation:
+// 1. Starting with input 4.
+// 2. Apply the last function: 4 - 3 = 1
+// 3. Apply the second function: 1 * 2 = 2
+// 4. Apply the first function: 2 + 1 = 3
