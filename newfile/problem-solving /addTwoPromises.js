eda1c9a4ec87d82358dafd969caae3a6a8bc1bf1
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-31 00:10:34 +0600

    update addTwoPromises.js comment the code

--- newfile/problem-solving /addTwoPromises.js
+++ newfile/problem-solving /addTwoPromises.js
@@ -1,3 +1,4 @@
+/*
 function addTwoPromises(promise1, promise2) {
   return Promise.all([promise1, promise2])
     .then(([result1, result2]) => result1 + result2);
@@ -9,3 +10,5 @@ const promise2 = Promise.resolve(10);
 addTwoPromises(promise1, promise2).then((sum) => {
   console.log(sum); // Output: 15
 });
+
+*/
