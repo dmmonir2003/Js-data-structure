f2a64bc7873cf88f68404349a7e341565253825f
dmmonir2003 <dmmonir2003@gmail.com>
2024-10-31 00:10:02 +0600

    update addTwoPromises.js test the code

--- newfile/problem-solving /addTwoPromises.js
+++ newfile/problem-solving /addTwoPromises.js
@@ -2,3 +2,10 @@ function addTwoPromises(promise1, promise2) {
   return Promise.all([promise1, promise2])
     .then(([result1, result2]) => result1 + result2);
 }
+
+const promise1 = Promise.resolve(5);
+const promise2 = Promise.resolve(10);
+
+addTwoPromises(promise1, promise2).then((sum) => {
+  console.log(sum); // Output: 15
+});
