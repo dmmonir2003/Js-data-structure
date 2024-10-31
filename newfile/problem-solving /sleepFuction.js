39b094df380123941dedaee84990718f117e03d9
dmmonir2003 <dmmonir2003@gmail.com>
2024-11-01 01:18:37 +0600

    update sleepFuction.js text the code

--- newfile/problem-solving /sleepFuction.js
+++ newfile/problem-solving /sleepFuction.js
@@ -1,3 +1,10 @@
 async function sleep(millis) {
   return new Promise(resolve => setTimeout(resolve, millis));
 }
+
+
+(async () => {
+  console.log('Start');
+  await sleep(1000); // Pauses for 1 second
+  console.log('End after 1 second');
+})();
