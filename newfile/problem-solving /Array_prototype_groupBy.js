6c09a7490b2f00fe04ad8d55413a209190db3d29
dmmonir2003 <dmmonir2003@gmail.com>
2024-11-21 22:00:31 +0600

    update Array_prototype_groupBy.js test the code

--- newfile/problem-solving /Array_prototype_groupBy.js
+++ newfile/problem-solving /Array_prototype_groupBy.js
@@ -1,4 +1,4 @@
-/ Enhance the Array prototype
+// Enhance the Array prototype
 Array.prototype.groupBy = function (fn) {
   return this.reduce((grouped, item) => {
     const key = fn(item); // Generate the key using the provided callback function
@@ -9,3 +9,14 @@ Array.prototype.groupBy = function (fn) {
     return grouped;
   }, {});
 };
+
+// Example Usage
+const arr = [1, 2, 3, 4, 5, 6];
+const grouped = arr.groupBy((num) => (num % 2 === 0 ? "even" : "odd"));
+console.log(grouped);
+// Output:
+// {
+//   odd: [1, 3, 5],
+//   even: [2, 4, 6]
+// }
+
